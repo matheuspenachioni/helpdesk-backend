f60be472a69b3ce08917a6ed48d7167e7b61fcae
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-01-05 22:40:11 -0300

    endpoint update for Tecnico

--- src/main/java/com/matheus/helpdesk/services/TecnicoService.java
+++ src/main/java/com/matheus/helpdesk/services/TecnicoService.java
@@ -14,6 +14,8 @@ import com.matheus.helpdesk.repositories.TecnicoRepository;
 import com.matheus.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.matheus.helpdesk.services.exceptions.ObjectNotFoundException;
 
+import jakarta.validation.Valid;
+
 @Service
 public class TecnicoService {
 	
@@ -40,6 +42,17 @@ public class TecnicoService {
 		
 		return repository.save(newObj);
 	}
+	
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		
+		Tecnico oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		
+		oldObj = new Tecnico(objDTO);
+		
+		return repository.save(oldObj);
+	}
 
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
@@ -55,4 +68,5 @@ public class TecnicoService {
 		}
 		
 	}
+	
 }
