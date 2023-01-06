8253cecd57093b3b808f7259b893a0de351acf1f
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-01-06 20:03:03 -0300

    status FECHAMENTO fixed

--- src/main/java/com/matheus/helpdesk/domain/enums/Status.java
+++ src/main/java/com/matheus/helpdesk/domain/enums/Status.java
@@ -2,7 +2,7 @@ package com.matheus.helpdesk.domain.enums;
 
 public enum Status {
 
-	ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), ENCERRADO(3, "ENCERRADO");
+	ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), ENCERRADO(2, "ENCERRADO");
 	
 	private Integer codigo;
 	private String descricao;
